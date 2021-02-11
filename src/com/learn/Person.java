1ae3f48b46122ccd5d4126e693ee439e64bb38a4
Priyanka <priyankadec16@yahoo.com>
2021-02-11 12:44:21 -0600

    first commit

---
+++ src/com/learn/Person.java
@@ -0,0 +1,34 @@
+package com.learn;
+
+public class Person {
+	//Fields
+	String name;
+	int age;
+	String nationality;
+	
+	
+	//methods
+	void get_person_details() {
+		
+		System.out.println("Name is "+name+", age is "+age+", nationality is "+nationality);
+		
+	}
+	
+
+
+}
+
+
+// data is nothing but a piece of information
+// all programs has to deal with some data
+// data_type - is a classification/categorization of what kind of data u r dealing with
+// simple - handling single values
+// complex - combination of several values
+
+// numbers  - int, float
+// text - char String 
+// boolean - boolean
+
+// x+y = 10
+// x = 5, y=5
+// x =3, y=7
